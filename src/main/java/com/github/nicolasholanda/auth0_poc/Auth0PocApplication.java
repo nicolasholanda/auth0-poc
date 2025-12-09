5f7c663b55d53f12e54458579b4bc21aa8e3d2a5
nicolasholanda <nikolas.nhm14@gmail.com>
2025-12-09 09:44:36 -0300

    initial commit

---
+++ src/main/java/com/github/nicolasholanda/auth0_poc/Auth0PocApplication.java
@@ -0,0 +1,13 @@
+package com.github.nicolasholanda.auth0_poc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Auth0PocApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Auth0PocApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/nicolasholanda/auth0_poc/Auth0PocApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.nicolasholanda.auth0_poc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Auth0PocApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
